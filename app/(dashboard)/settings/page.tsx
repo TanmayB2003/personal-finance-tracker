b97708e31c9bcc712450f30e702372e7c064d18f
TanmayB2003 <tanmaybhatnagar@outlook.com>
2025-07-01 22:51:06 +0530

    notify settings dev

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,5 +1,18 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 const SettingsPage = () => {
-  return <>Coming soon</>;
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Settings</CardTitle>
+        </CardHeader>
+        <CardContent>
+          Coming Soon!!
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default SettingsPage;
